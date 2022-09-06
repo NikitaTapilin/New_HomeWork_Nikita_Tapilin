d9edd2e9156acf651e926c0237eef018734cb290
NikitaTapilin <tapilinn@gmail.com>
2022-09-06 17:58:49 +0400

    updated 2 task

--- 3_Csharp/1_lesson/2_task/max_num.cs
+++ 3_Csharp/1_lesson/2_task/max_num.cs
@@ -4,17 +4,9 @@ Console.Write("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2){
-    if (num1 > num3){
-        Console.WriteLine("Наибольшее число: " + num1);
-    }
-    else if (num1 < num3){
-        Console.WriteLine("Наибольшее число: " + num3);
-    }
-}
-else if (num2 > num3){
-    System.Console.WriteLine("Наибольшее число: " + num2);
-}
-else{
-    Console.WriteLine("Наибольшее число: " + num3);
-}
+
+int max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("Наибольшее число: " + max);
